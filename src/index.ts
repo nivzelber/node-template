8cc1527163dc1d8513741117c65ba1dfc9a8ecfd
Ariel Mizrahi <Ariellmm2@gmail.com>
2023-03-21 20:31:03 +0200

    api Crud and Mongo connection

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import { logger } from "./logger";
-import { createListing } from "./mongo";
+import {
+  createListing,
+  deleteOneListing,
+  findListingByName,
+  findMultListing,
+  updateListingByName
+} from "./mongo";
 
 const app = express();
 // parse application/x-www-form-urlencoded
@@ -11,15 +17,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/get", (req, res) => {
+//first get opp is for read object by name , and second one is for read objects by date
+app.get("/get1", (req, res) => {
   logger.info("new Get Req");
-  return res.status(200).send("Hii");
+  findListingByName(req.query.name);
+  return res.status(200).send("Hii1");
+});
+app.get("/get2", (req, res) => {
+  logger.info("new Get Req");
+  findMultListing(req.query.date);
+  return res.status(200).send("Hii2");
 });
 
-app.post("/user", (req, res) => {
+//post req - create new object to mongoDB
+app.post("/newUser", (req, res) => {
   logger.info("new Post Req");
   createListing({ name: req.body.name, lastName: req.body.lastName, date: req.body.date });
   return res.status(200).send("post");
+});
+
+app.put("/update", (req, res) => {
+  logger.info("new Update Req");
+  updateListingByName(req.body.name, req.body.value);
+  logger.info(` ${req.body.name}, ${req.body.value}`);
+  return res.status(200).send("update");
+});
+
+app.delete("/delete", (req, res) => {
+  logger.info("new Delete Req");
+  deleteOneListing(req.body.name);
+  return res.status(200).send("delete");
 });
 
 app.listen(5000, () => {
--- src/mongo.ts
+++ src/mongo.ts
@@ -14,23 +14,26 @@ try {
 //     //await createListing(client, { name: "Ariel", lastName: "Miz", date: "24/02/1999" });
 //     //await findListingByName(client, "Ariel");
 //     //await findMultListing(client, { date: "24/02/1999" });
+// await updateListingByName('old name' , 'new name');
 //     //await deleteOneListing(client, "Ori");
 
 export async function createListing(newListing) {
   const result = await client.db("RestManagerDB").collection("users").insertOne(newListing);
-
   console.log(`new listing created with following id : ${result.insertedId}`);
 }
 
 export async function findMultListing(prop) {
   // find multiplay objects by date - return whole object
   const cursor = await client.db("RestManagerDB").collection("users").find({ date: prop });
-
   const result = await cursor.toArray();
-  console.log(`Found listing with the date mention : ${prop}`);
-  result.forEach(result => {
-    console.log(result);
-  });
+  if (result) {
+    console.log(`Found listing with the date mention : ${prop}`);
+    result.forEach(result => {
+      console.log(result);
+    });
+  } else {
+    console.log(`NOT! Found listing with the date mention : ${prop}`);
+  }
 }
 
 export async function findListingByName(nameOfListing) {
@@ -50,7 +53,7 @@ export async function updateListingByName(nameOfListing, value) {
   const result = await client
     .db("RestManagerDB")
     .collection("users")
-    .updateOne({ name: nameOfListing }, { $set: value });
+    .updateOne({ name: nameOfListing }, { $set: { name: value } });
   console.log(`${result.matchedCount} document(s) matched the query criteria`);
   console.log(`${result.modifiedCount} documents was/were updated`);
 }
